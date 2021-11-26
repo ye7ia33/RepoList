7fa32a71cbb593072c8a9eec2aa3184b3087a0f8
Yahia El-Dow <yahia.eldow@gmail.com>
2021-11-26 10:58:02 +0200

    Initial Commit

---
+++ RepoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RepoList
+//
+//  Created by Yahia El-Dow on 26/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RepoListUITests/RepoListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RepoListUITestsLaunchTests.swift
+//  RepoListUITests
+//
+//  Created by Yahia El-Dow on 26/11/2021.
+//
+
+import XCTest
+
+class RepoListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
